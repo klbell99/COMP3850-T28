a1bfc9f43c6c16acbe38d25f1b49c750d5900261
klbell99 <69019993+klbell99@users.noreply.github.com>
2023-09-03 16:35:58 +1000

    Added fire sound, fixed blink texture issue
    Also moved fire/camera to the right and created week 6 build, added a debug script for changing the lighting

---
+++ Assets/Scripts/DebugObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugObject : MonoBehaviour
+{
+    public GameObject defaultLight;
+    public GameObject fireLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Oculus_GearVR_RIndexTrigger") || Input.GetButtonDown("Fire1")) {
+            if (defaultLight.activeInHierarchy) {
+                defaultLight.SetActive(false);
+                fireLight.SetActive(true);
+            }
+            else {
+                defaultLight.SetActive(true);
+                fireLight.SetActive(false);
+            }
+        }
+    }
+}
