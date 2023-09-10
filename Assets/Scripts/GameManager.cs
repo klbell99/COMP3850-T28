05b37a461816b47108fe0fddbe4edd63f2f36740
klbell99 <69019993+klbell99@users.noreply.github.com>
2023-09-10 15:03:53 +1000

    basic gamemanager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject blinkObj;
+    public float dimTime;   //in minutes
+    public float endTime;   //in minutes
+    private float elapsedTime;  //in seconds
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsedTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+    }
+}
