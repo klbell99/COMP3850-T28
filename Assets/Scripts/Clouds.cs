fe842d14da38158fb89cef7753590a00dd055341
klbell99 <69019993+klbell99@users.noreply.github.com>
2023-09-25 10:32:28 +1000

    Skybox changed, textures for tent+bag+bark
    Also moved some of the skybox textures around in the folder

---
+++ Assets/Scripts/Clouds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clouds : MonoBehaviour
+{
+    public float rotationSpeed;
+    private float thisRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thisRotation = rotationSpeed * Time.deltaTime;
+        this.gameObject.transform.Rotate(thisRotation, 0f, 0f);
+    }
+}
