9991c24eb0b867dce8d782df445ea3b409f70ac5
klbell99 <69019993+klbell99@users.noreply.github.com>
2023-10-04 23:33:09 +1100

    Lowered resolutino on skybox images

---
+++ Assets/Scripts/AddMallow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddMallow : MonoBehaviour
+{
+
+    public bool isHead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (isHead) {
+            other.GetComponent<Prototype>().MallowDisappear();
+        } else {
+            other.GetComponent<Prototype>().MallowAppear();
+        }
+    }
+}
