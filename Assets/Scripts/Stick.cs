f823fdfb62560ff34469ba9f39e2c76d88547334
klbell99 <69019993+klbell99@users.noreply.github.com>
2023-10-09 01:54:54 +1100

    Revert "Revert "Week 9 build""
    This reverts commit 3d10a6480add88b1413937d22b0703cea27a4528.

--- Assets/Scripts/Stick.cs
+++ Assets/Scripts/Stick.cs
@@ -16,12 +16,15 @@ public class Stick : MonoBehaviour
     private float toastAlpha;       // current toast texture alpha value
 
     private MarshmallowState mallowState;
-    private enum MarshmallowState
+    public enum MarshmallowState
     {
         None,
         Raw,
         Cooked,
         Burnt
+    }
+    public MarshmallowState _CurrentState{
+        get { return mallowState; }
     }
     // Start is called before the first frame update
     void Start()
@@ -83,6 +86,7 @@ public class Stick : MonoBehaviour
             }
         }
         if (toastTime > meltThreshold && mallowState == MarshmallowState.Burnt) {
+            FireCollider.Instance.StopSound();
             MallowDisappear();
         }
     }
