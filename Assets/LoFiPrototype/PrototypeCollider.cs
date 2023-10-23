7c830a58bc4f9a1e472d579322d3934ee46367f6
olisugo <79487246+olisugo@users.noreply.github.com>
2023-10-23 12:17:32 +1100

    exported terrain to obj

--- Assets/LoFiPrototype/PrototypeCollider.cs
+++ Assets/LoFiPrototype/PrototypeCollider.cs
@@ -1,41 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class PrototypeCollider : MonoBehaviour
-{
+//public class PrototypeCollider : MonoBehaviour
+//{
 
-    public bool isFire;
-    public bool isHead;
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (isFire) {
-            this.gameObject.GetComponent<Renderer>().material.color = Color.red;
-        }
-        if (!isHead && !isFire) {
-            this.gameObject.GetComponent<Renderer>().material.color = Color.blue;
-        }
-    }
+//    public bool isFire;
+//    public bool isHead;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        if (isFire) {
+//            this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+//        }
+//        if (!isHead && !isFire) {
+//            this.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+//        }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
         
-    }
+//    }
 
-    void OnTriggerEnter(Collider other) {
-        if (!isFire && !isHead) {
-            other.GetComponent<Prototype>().MallowAppear();
-        }
-        if (isHead) {
-            other.GetComponent<Prototype>().MallowDisappear();
-        }
-    }
+//    void OnTriggerEnter(Collider other) {
+//        if (!isFire && !isHead) {
+//            other.GetComponent<Prototype>().MallowAppear();
+//        }
+//        if (isHead) {
+//            other.GetComponent<Prototype>().MallowDisappear();
+//        }
+//    }
 
-    void OnTriggerStay(Collider other) {
-        if (isFire) {
-            other.GetComponent<Prototype>().MallowToast();
-        }
-    }
-}
+//    void OnTriggerStay(Collider other) {
+//        if (isFire) {
+//            other.GetComponent<Prototype>().MallowToast();
+//        }
+//    }
+//}
