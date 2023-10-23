d7958eebf4a5dbea090989c5def7c65e0f5302e6
olisugo <79487246+olisugo@users.noreply.github.com>
2023-10-23 12:23:25 +1100

    Revert "exported terrain to obj"
    This reverts commit 7c830a58bc4f9a1e472d579322d3934ee46367f6.

--- Assets/LoFiPrototype/Prototype.cs
+++ Assets/LoFiPrototype/Prototype.cs
@@ -1,50 +1,50 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class Prototype : MonoBehaviour
-//{
-//    public GameObject mallow;
-//    private float toastTime = 0;
-//    private bool toasted = false;
-//    private bool burned = false;
-//    // Start is called before the first frame update
-//    void Start()
-//    {
+public class Prototype : MonoBehaviour
+{
+    public GameObject mallow;
+    private float toastTime = 0;
+    private bool toasted = false;
+    private bool burned = false;
+    // Start is called before the first frame update
+    void Start()
+    {
         
-//    }
+    }
 
-//    // Update is called once per frame
-//    void Update()
-//    {
+    // Update is called once per frame
+    void Update()
+    {
         
-//    }
+    }
 
-//    public void MallowAppear() {
-//        toastTime = 0;
-//        toasted = false;
-//        burned = false;
-//        mallow.GetComponent<Renderer>().material.color = Color.white;
-//        mallow.SetActive(true);
-//    }
+    public void MallowAppear() {
+        toastTime = 0;
+        toasted = false;
+        burned = false;
+        mallow.GetComponent<Renderer>().material.color = Color.white;
+        mallow.SetActive(true);
+    }
 
-//    public void MallowDisappear() {
-//        mallow.SetActive(false);
-//    }
+    public void MallowDisappear() {
+        mallow.SetActive(false);
+    }
 
-//    public void MallowToast() {
-//        toastTime += Time.deltaTime;
-//        if (toastTime > 10 && !toasted) {
-//            toasted = true;
-//            mallow.GetComponent<Renderer>().material.color = Color.yellow;
-//        }
-//        if (toastTime > 20 && !burned) {
-//            burned = true;
-//            MallowBurn();
-//        }
-//    }
+    public void MallowToast() {
+        toastTime += Time.deltaTime;
+        if (toastTime > 10 && !toasted) {
+            toasted = true;
+            mallow.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        if (toastTime > 20 && !burned) {
+            burned = true;
+            MallowBurn();
+        }
+    }
 
-//    void MallowBurn() {
-//        mallow.GetComponent<Renderer>().material.color = Color.black;
-//    }
-//}
+    void MallowBurn() {
+        mallow.GetComponent<Renderer>().material.color = Color.black;
+    }
+}
